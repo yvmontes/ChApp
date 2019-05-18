80f21fa8799e7aa107b5ddb6804404a39ae3af75
loopy muchacha <lupegdealba@gmail.com>
2019-05-18 02:23:41 -0700

    lupe

--- public/js/index.js
+++ public/js/index.js
@@ -31,30 +31,6 @@ function process(e) {
 //     alert('You clicked me...!');
 //   });
 
-<<<<<<< HEAD
-// var activeLink;
-// function changeUsersDiv (active){
-//     console.log(active);
-//     if (active !== undefined) {
-//         toggleDisplay();
-//         activeLanguage = active;
-//         translateButtonClicked();
-//     }
-
-
-// switch (active) {
-
-//   case 'users':
-//       console.log(activeLanguage);
-//       document.getElementById("users-div").innerHTML = "Currently showing users";
-//       break;
-//   case 'rooms':
-//       document.getElementById("users-div").innerHTML = "Currently showing rooms";
-//   default:
-//       break;
-// }
-// }
-=======
 //   //press enter on text area..
 //  $('#textbox').keypress(function (e) {
 //  var key = e.which;
@@ -68,7 +44,6 @@ function process(e) {
 // });
 
 // changes the users-div to users or rooms
->>>>>>> a428bd50fe8872f0a77ba8900fd6c2c5b024e9c9
 
 var usersBox = document.getElementById("usersDiv");
 var usersContent = document.getElementById("users");
@@ -85,25 +60,6 @@ rooms.onclick = function() {
 
 // back button
 
-<<<<<<< HEAD
-rooms.onclick = function(){
-  document.getElementById("users-div").innerHTML = "Currently showing rooms";
-}
-
-
-// CODE FOR PREPENDING CHAT INTO COLUMN
-window.onload=function(){
-var form = document.getElementsByTagName("form")[0];
-form.onsubmit = function(e){
-    e.preventDefault();
-    var messege = document.getElementsByName("messege")[0].value;
-    var newDiv = document.createElement("div");
-    newDiv.className = "message";
-    newDiv.innerHTML = "<span style='color: red;'>Hawkgirl:</span>" + "<span>" + messege + "</span>";
-    document.getElementById("chatwindow-div").prepend(newDiv);
-}
-=======
 function goBack() {
     window.history.back();
->>>>>>> a428bd50fe8872f0a77ba8900fd6c2c5b024e9c9
 }
